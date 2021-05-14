8aad61bf1bc9e4328c45c82c6ee339f36e892693
JuliandresCanon <JuliandresCanon@gmail.com>
2021-05-13 19:25:27 -0500

    Clase 9

---
+++ Ejercicios/clase_9.2.py
@@ -0,0 +1,12 @@
+#Calculo de costos arriendo de Vehiculo
+
+km = int(input("Ingrese la distancia recorrida: ")
+
+if km < 300:
+    pago = 300000
+elif km > 300 and km < 1000:
+    pago = 300000 + 15000*(km - 300)
+else:
+    pago = 300000 + 15000*(700) + 10000*(km - 1000)
+
+print("El costo del alquiler es: $", pago)
