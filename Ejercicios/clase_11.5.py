95dc650af4537c6e49a1b6cf2eda666df25caffe
JuliandresCanon <JuliandresCanon@gmail.com>
2021-05-18 20:03:40 -0500

    Clase 11

---
+++ Ejercicios/clase_11.5.py
@@ -0,0 +1,50 @@
+precio_manzana = 2500
+cant_manzana = 5
+precio_panes = 1500
+cant_panes = 3
+precio_salchichas = 1200
+cant_salchichas = 7
+precio_salsas = 3000
+cant_salsas = 2
+
+subtotal = 0
+cantidad = 0
+
+print("Calculando el total del mercado... ")
+
+total_manzana = precio_manzana * cant_manzana
+print("El valor total de las manzanas es: $" + str(total_manzana))
+subtotal = subtotal + total_manzana
+print("... El Subtotal sería de: $"+ str(subtotal))
+cantidad = cantidad + cant_manzana
+print("Se han comprado "+ str(cantidad) + " Productos")
+
+total_panes = precio_panes * cant_panes
+print("El valor total del panes es: $" + str(total_panes))
+subtotal = subtotal + total_panes
+print("... El Subtotal sería de: $"+ str(subtotal))
+cantidad = cantidad + cant_panes
+print("Se han comprado "+ str(cantidad) + " Productos")
+
+
+total_salchichas = precio_salchichas * cant_salchichas
+print("El valor total del salchichas es: $" + str(total_salchichas))
+subtotal = subtotal + total_salchichas
+print("... El Total de mercado es de: $"+ str(subtotal))
+cantidad = cantidad + cant_salchichas
+print("Se han comprado "+ str(cantidad) + " Productos")
+
+total_salsas = precio_salsas * cant_salsas
+print("El valor total del salsas es: $" + str(total_salsas))
+subtotal = subtotal + total_salsas
+print("... El Total de mercado es de: $"+ str(subtotal))
+cantidad = cantidad + cant_salsas
+print("Se han comprado "+ str(cantidad) + " Productos")
+
+pago = int(input("Ingrese el monto recibido para el pago: "))
+vueltas = pago - subtotal
+
+if vueltas > 0:
+    print("El pago fue realizado efectivamente: \n El monto a devolver es: $" + str(vueltas))
+else:
+    print("El pago no fue realizado efectivamente: \n El monto recibido es insuficiente, faltan $"+ str(vueltas*-1))
---
+++ reto_3.py
@@ -0,0 +1,46 @@
+
+# Una empresa consultora de bebidas realizó un estudio sobre la temperatura ideal que debería tener 
+# un café colombiano para un buen sabor. Se encontró que la temperatura ideal es de 202 grados Fahrenheit 
+# y cada segundo que pasa el café a temperatura ambiente se enfría 1 grado Fahrenheit.
+
+# Tu cafetería desea usar la información del estudio para ofrecer el mejor café de la ciudad, sin embargo 
+# la máquina que lo realiza sólo trabaja a grados Celsius y produce café que tiene temperatura aleatoria 
+# entre 103.3 y 119.3 grados Celsius.
+
+# Planteamiento del reto
+
+#¿De qué manera podrías automatizar el cálculo de cuánto tiempo debería esperar el mesero para que el café 
+# tenga la temperatura ideal cuando llegue al cliente?
+
+# Nota: La máquina muestra la temperatura que tiene el café que produce.
+
+
+# Acciones de aprendizaje
+
+# 1. Determinar desde las variables identificadas, cual(es) corresponden a los datos de entrada, las operaciones 
+# entre ellas que dan solución al reto, y cual(es) son los datos para presentar como salida
+
+# 2. Analizar, identificar y declarar las variables que considere necesarias para realizar los cálculos de la 
+# transformación de temperatura
+
+# 3. Diseñar el algoritmo desde un lenguaje de programación que facilite las tareas de automatización, depuración 
+# y verificación de la solución propuesta.
+
+# Solución del reto
+
+# El aplicativo debe cumplir los siguientes requerimientos:
+
+# 1. La entrada será la temperatura en Celsius. El tipo será float
+
+# 2. El paso del tiempo se debe implementar usando un bucle iterativo.
+
+# 3. La salida será la impresión de la temperatura del café segundo a segundo hasta llegar a la ideal.
+
+#Solución del reto
+
+# Solucione el reto, escribiendo el código que represente la automatización del cálculo de cuánto tiempo debería 
+# esperar el mesero para que el café tenga la temperatura ideal cuando llegue al cliente.
+
+temp_c=float(input("Ingrese la Temperatura: "))
+
+
