c9959867c392d85e2cde6a56e2d9879e2fe0eafc
JuliandresCanon <JuliandresCanon@gmail.com>
2021-05-27 19:09:46 -0500

    Clase 18

---
+++ Ejercicios/clase_18.2.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+
+image = img.imread('C:\Users\TCI-JULIAN\GitHub\MISION_TIC_2022\Ejercicios\math-comb.jpg')
+plt.imshow(image)
+plt.show()
