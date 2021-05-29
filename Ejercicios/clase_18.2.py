820d34e96c7389834ef7ea78a749fa2dbe5c0cfc
JuliandresCanon <JuliandresCanon@gmail.com>
2021-05-28 20:11:07 -0500

    CÃlase 19

--- Ejercicios/clase_18.2.py
+++ Ejercicios/clase_18.2.py
@@ -4,3 +4,5 @@ import matplotlib.image as img
 image = img.imread('C:\Users\TCI-JULIAN\GitHub\MISION_TIC_2022\Ejercicios\math-comb.jpg')
 plt.imshow(image)
 plt.show()
+
+# https://colab.research.google.com/drive/1Rf7dI0_sFVXh8Kyob4Ef1nDlYiZqB0-E?usp=sharing#scrollTo=CkmVrWNUavne
