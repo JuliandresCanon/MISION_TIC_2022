40e7709547f20c99a858cd632dea2d4427e22929
JuliandresCanon <JuliandresCanon@gmail.com>
2021-05-31 09:08:23 -0500

    Reto 4

--- reto_4.py
+++ reto_4.py
@@ -1,4 +1,4 @@
-def CalculoCostoHuevos(cantAAA, cantAA, cantA, cantB, cantC):
+def calculoCostoHuevos(cantAAA, cantAA, cantA, cantB, cantC):
     
     costoAAA = cantAAA * 550
     costoAA = cantAA * 450
@@ -7,7 +7,6 @@ def CalculoCostoHuevos(cantAAA, cantAA, cantA, cantB, cantC):
     costoC = cantC * 100
     costo_total = costoAAA + costoAA + costoA + costoB + costoC
     
-    return(" costoAAA: " + str(costoAAA) + " costoAA: " + str(costoAA) + " costoA: " + str(costoA) + " costoB: " + str(costoB) + " costoC: " + str(costoC) + " costo total: " + str(costo_total))
+    return("costoAAA:" + str(costoAAA) + " costoAA:" + str(costoAA) + " costoA:" + str(costoA) + " costoB:" + str(costoB) + " costoC:" + str(costoC) + " costo total:" + str(costo_total))
     
-
-print(CalculoCostoHuevos(1,2,3,4,5))
+print(calculoCostoHuevos(1,2,3,4,5))
