b583e71c1c243ca6494ae562e0fee142e1fe4e64
JuliandresCanon <JuliandresCanon@gmail.com>
2021-06-09 19:11:31 -0500

    Reto 5 con diccionarios

--- Retos/reto_5.2.py
+++ Retos/reto_5.2.py
@@ -2,19 +2,19 @@
 # Reto tema 5
 
 # Define una función que reciba como parámetros una cadena de texto con el siguiente formato:
-# "Enero,4,3,4;Febrero,4,3;Marzo,4,3,4;Abril,4,3,4,4" para los registros de ahorro 
-# y un segundo parámetro con el nombre de la persona Por ejemplo:
+# Enero,40000,35000,80000;Febrero,90000,30000;Marzo,50000,35000,25000;Abril,75000,35000,42000,38000
+# para los registros de ahorro y un segundo parámetro con el nombre de la persona Por ejemplo:
 # calculadoraMes(registroDeAhorros,nombre)
 
 # Retorna la respuesta con el siguiente formato por ejemplo para las entradas 
-# calculadoraMes("Enero,4,3,4;Febrero,4,3;Marzo,4,3,4;Abril,4,3,4,4" ,"daniel")  
-# la salida sería ('daniel', {'Enero': 11, 'Febrero': 7, 'Marzo': 11, 'Abril': 15})
+# calculadoraMes("Enero,40000,35000,80000;Febrero,90000,30000;Marzo,50000,35000,25000;Abril,75000,35000,42000,38000" ,"daniel")
+# la salida sería ('daniel', {'Enero': 155000, 'Febrero': 120000, 'Marzo': 110000, 'Abril': 190000})
 
 
 def calculadoraMes(registroDeAhorros, nombre):
 
     registroMes = registroDeAhorros.split(";")
-    resultado = set()
+    resultado = {}
     
     for n in range(len(registroMes)):
         datosMes = registroMes[n].split(",")
@@ -23,10 +23,11 @@ def calculadoraMes(registroDeAhorros, nombre):
         for m in range(1, len(datosMes)):
             aportes += int(datosMes[m])
             
-        resultado.add(datosMes[0]+": "+str(aportes))
+        resultado[datosMes[0]] = aportes
         
     return nombre, resultado
 
 
-print(calculadoraMes("Enero,4,3,4;Febrero,4,3;Marzo,4,3,4;Abril,4,3,4,4", "daniel"))
-print("('daniel', {'Enero': 11, 'Febrero': 7, 'Marzo': 11, 'Abril': 15})")
+print(calculadoraMes("Enero,40000,35000,80000;Febrero,90000,30000;Marzo,50000,35000,25000;Abril,75000,35000,42000,38000", "daniel"))
+
+print("('daniel', {'Enero': 155000, 'Febrero': 120000, 'Marzo': 110000, 'Abril': 190000})")
